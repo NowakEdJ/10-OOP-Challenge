659819ade53f2e404a8e71b17efd3f7eaa4053b5
Ed Nowak <ed@7pointsolutions.com>
2024-01-04 13:47:49 -0700

    created shape classes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 
 // User input questions
 const questions = [
@@ -13,39 +14,34 @@ const questions = [
 	{
 		type: 'input',
 		name: 'shapeColor',
-		message: 'Enter a shape background color (keyword or hex).'
+		message: 'Enter a shape background color (keyword or hex):'
 	},
 	{
 		type: 'input',
 		name: 'initials',
-		message: 'Enter up to 3 characters.'
+		message: 'Enter up to 3 characters:'
 	},
 	{
 		type: 'input',
 		name: 'textColor',
-		message: 'Enter a text color (keyword or hex).'
+		message: 'Enter a text color (keyword or hex):'
 	}
 ];
 
-// Generate SVG
+// Generates SVG
 function generateSVG(shape, shapeColor, initials, textColor) {
-	let svgShape = '';
-	switch(shape) {
-			case 'Circle':
-					svgShape = `<circle cx="50" cy="50" r="40" fill="${shapeColor}" />`;
-					break;
-			case 'Square':
-					svgShape = `<rect width="80" height="80" fill="${shapeColor}" />`;
-					break;
-			case 'Triangle':
-					svgShape = `<polygon points="50,10 10,90 90,90" fill="${shapeColor}" />`;
-					break;
+	let shapeObject;
+
+	if (shape === 'Circle') {
+		shapeObject = new Circle(shapeColor);
+	} else if (shape === 'Square') {
+		shapeObject = new Square(shapeColor);
+	} else if (shape === 'Triangle') {
+		shapeObject = new Triangle(shapeColor);
 	}
 
-	// Add initials
+	const svgShape = shapeObject.getSVG();
 	const svgInitials = `<text x="50%" y="55%" fill="${textColor}" font-size="20" text-anchor="middle">${initials}</text>`;
-
-	// Combine everything
 	return `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">${svgShape}${svgInitials}</svg>`;
 }
 
@@ -59,7 +55,7 @@ function init() {
 	inquirer
   	.prompt(questions).then((dataFromInquirer) => {
 			const { shape, shapeColor, initials, textColor } = dataFromInquirer;
-			outputSVG('./examples/output.svg', generateSVG(shape, shapeColor, initials, textColor));
+			outputSVG('./examples/logo.svg', generateSVG(shape, shapeColor, initials, textColor));
 		})
 }
 
